7afa8be02dbca91fbd778bc69eb1d09fa091930e
Mariano Gappa <mariano@movio.co>
2017-07-16 22:33:53 +1200

    Initial commit.

---
+++ kmeans.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"math"
+	"math/rand"
+)
+
+type centroid struct {
+	center []float64
+	ps     [][]float64
+}
+
+func d(p1, p2 []float64) float64 {
+	sum := float64(0)
+	for e := 0; e < len(p1); e++ {
+		sum += math.Pow(p1[e]-p2[e], 2)
+	}
+	return math.Sqrt(sum)
+}
+
+func (c *centroid) recenter() float64 {
+	newC := make([]float64, len(c.center))
+	for _, e := range c.ps {
+		for r := 0; r < len(newC); r++ {
+			newC[r] += e[r]
+		}
+	}
+	for r := 0; r < len(newC); r++ {
+		newC[r] /= float64(len(c.ps))
+	}
+	oldCenter := c.center
+	c.center = newC
+	return d(oldCenter, c.center)
+}
+
+func kmeans(data [][]float64, k uint64, deltaThreshold float64) (centroids []centroid) {
+	for i := uint64(0); i < k; i++ {
+		centroids = append(centroids, centroid{center: data[rand.Intn(len(data))]})
+	}
+
+	for {
+		for i := range data {
+			minDist := math.MaxFloat64
+			z := 0
+			for v, e := range centroids {
+				dist := d(data[i], e.center)
+				if dist < minDist {
+					minDist, z = dist, v
+				}
+			}
+			centroids[z].ps = append(centroids[z].ps, data[i])
+		}
+		maxDelta := -math.MaxFloat64
+		for i := range centroids {
+			delta := centroids[i].recenter()
+			if delta > maxDelta {
+				maxDelta = delta
+			}
+		}
+		if deltaThreshold >= maxDelta {
+			return
+		}
+		for i := range centroids {
+			centroids[i].ps = nil
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+type freq struct {
+	i, c int
+}
+
+func generateFreqs(in []string) map[string]freq {
+	var (
+		fs = make(map[string]freq)
+		j  = -1
+	)
+	for _, i := range in {
+		for _, w := range strings.Split(i, " ") {
+			if _, ok := fs[w]; !ok {
+				j++
+				fs[w] = freq{j, 1}
+			} else {
+				t := fs[w]
+				t.c++
+				fs[w] = t
+			}
+		}
+	}
+	return fs
+}
+
+// features are:
+// 1. [] frequency one hot vector
+// 2. sentence length
+// 3. index of first word in wordmap
+// 4. index of last word in wordmap
+// 5. [] index of each word in sentence
+func generateOneHots(in []string, fs map[string]freq) [][]float64 {
+	var ohs = make([][]float64, len(in))
+	for i := range ohs {
+		ohs[i] = make([]float64, len(fs)+3)
+	}
+
+	var maxWordCount = 0
+	for i := range in {
+		var split = strings.Split(in[i], " ")
+		for j, w := range split {
+			var f = fs[w]
+			ohs[i][f.i] = float64(f.c) // 1.
+			if j == 0 {
+				ohs[i][len(fs)+1] = float64(fs[w].i) // 3.
+			}
+			if j == len(split)-1 {
+				ohs[i][len(fs)+2] = float64(fs[w].i) // 4.
+			}
+		}
+		if len(split) > maxWordCount {
+			maxWordCount = len(split)
+		}
+		ohs[i][len(fs)] = float64(len(in[i])) // 2.
+	}
+
+	for i := range in {
+		var (
+			split = strings.Split(in[i], " ")
+			pos   = 0
+		)
+		for j := 0; j < maxWordCount; j++ {
+			if len(split)-1 < j {
+				ohs[i] = append(ohs[i], float64(0))
+				continue
+			}
+			ohs[i] = append(ohs[i], float64(pos))
+			pos += len(split[j]) + 1
+		}
+	}
+
+	return ohs
+}
+
+func readInput() []string {
+	var (
+		ls  = make([]string, 0, 500)
+		rd  = bufio.NewReader(os.Stdin)
+		l   string
+		err error
+	)
+	for {
+		l, err = rd.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		l = strings.TrimSpace(l)
+		if len(l) == 0 {
+			continue
+		}
+		ls = append(ls, l)
+	}
+	return ls
+}
+
+func main() {
+	var (
+		k      = flag.Uint64("k", 5, "how many clusters")
+		short  = flag.Bool("short", false, "show one representative row per cluster, with cardinality")
+		random = flag.Bool("random", false, "sets the random seed to current time (otherwise deterministic)")
+	)
+
+	flag.Parse()
+
+	if *random {
+		rand.Seed(time.Now().UTC().UnixNano())
+	}
+
+	var (
+		in  = readInput()
+		fs  = generateFreqs(in)
+		ohs = generateOneHots(in, fs)
+		cs  = kmeans(ohs, *k, 0.001)
+	)
+	for i := range cs {
+		maxSumFs, maxSumFsI := 0, 0 // sentence in cluster with maximum aggregate frequency
+		for _, p := range cs[i].ps {
+			var inI int
+		ohs:
+			for j, oh := range ohs { // find the sentence (in[j]) from the feature vector (cs[i].ps)
+				for l, c := range oh {
+					if c != p[l] {
+						continue ohs
+					}
+				}
+				if !*short {
+					fmt.Printf("Cluster %v\t%v\n", i, in[j])
+				}
+				inI = j
+				break
+			}
+
+			if *short {
+				sumFs := 0 // calculate sentence aggregate frequency
+				for _, f := range p[0:len(fs)] {
+					sumFs += int(f)
+				}
+				if sumFs > maxSumFs { // find sentence with maximum aggregate frequency
+					maxSumFs, maxSumFsI = sumFs, inI
+				}
+			}
+		}
+		if *short && len(cs[i].ps) > 0 {
+			fmt.Printf("%v\t%v\n", len(cs[i].ps), in[maxSumFsI])
+		}
+	}
+}
